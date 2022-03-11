639b6756b86be2656aa02121a45e970914a4c1ba
chrisjsimpson <chris.j.simpson@live.co.uk>
2022-03-11 23:35:29 +0000

    Fix #4 send logs live into mongo simulate

--- README.md
+++ README.md
@@ -31,10 +31,46 @@ Pretty crude/basic fill of database:
 python seeddb.py
 ```
 
-(Based on [mongodb article](https://www.mongodb.com/developer/how-to/seed-database-with-fake-data/)
-converted to python).
+## Simulate sending live http logs to mongodb
+
+- Logs get written to database name `logs`
+- Into collection named: `rawLogs`
+
+Start generating live logs (fake logs)
+```
+log-generator log-generator.conf
+```
+
+In another terminal, send logs to mongo (like `tail -f`)
+
+```
+python send-live-logs.py
+```
+
+Sample result:
+```
+rs03:PRIMARY> db.rawLogs.findOne()
+{
+	"_id" : ObjectId("622bd6183dceeab8bc0e2439"),
+	"rawLog" : "108.132.11.137 - - [11/Mar/2022:22:47:40 +0000] \"GET /lists HTTP/1.1\" 200 3462\n"
+}
+rs03:PRIMARY> show dbs
+admin          0.001GB
+config         0.000GB
+iot            0.147GB
+local          0.208GB
+logs           0.001GB
+test_database  0.000GB
+```
+
+
+
+
+
 
 
 ## References
 
 - (old) blog https://blog.karmacomputing.co.uk/how-to-create-mongodb-replica-set-with-authentication-ubuntu-16-04/
+(Based on [mongodb article](https://www.mongodb.com/developer/how-to/seed-database-with-fake-data/)
+converted to python).
---
+++ send-live-logs.py
@@ -0,0 +1,56 @@
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+from faker import Faker
+
+# tail Credit https://stackoverflow.com/a/12523371
+import time
+import subprocess
+import select
+
+# Based on:
+# https://www.mongodb.com/developer/how-to/seed-database-with-fake-data/
+# Converted to python
+
+load_dotenv(verbose=True)  # take environment variables from .env.
+
+fake = Faker()
+
+MONGO_DATABASE_URI = os.getenv("MONGO_DATABASE_URI")
+
+
+def getClient():
+    """Return mongodb client"""
+    uri = MONGO_DATABASE_URI
+    client = MongoClient(uri)
+    return client
+
+
+def seedDB():
+    # tail -f a log file and send it to mongodb
+    client = getClient()
+    # Delete everything
+    # collection.drop()
+    f = subprocess.Popen(
+        ["tail", "-F", "apache.log"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,  # noqa: E501
+    )
+    p = select.poll()
+    p.register(f.stdout)
+
+    while True:
+        if p.poll(1):
+            newLine = f.stdout.readline()
+            print(newLine)
+            time.sleep(0.05)
+            # Raw logs
+            db = client.logs
+            collection = db.rawLogs
+            rawLog = {"rawLog": newLine}
+            print(f"Sending new live log to mongo: {rawLog}")
+            collection.insert_one(rawLog)
+
+
+seedDB()
