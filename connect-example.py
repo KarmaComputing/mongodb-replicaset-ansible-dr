95e4963dc78521a8675cf9bf1839ad226a1b9a15
Joel <joel@karmacomputing.co.uk>
2022-03-11 18:05:01 -0400

    adjusting script

--- connect-example.py
+++ connect-example.py
@@ -28,23 +28,17 @@ fake.add_provider(database_provider)
 load_dotenv(verbose=True)  # take environment variables from .env.
 
 MONGO_DATABASE_URI = os.getenv("MONGO_DATABASE_URI")
-MONGO_SLAVE_URI = os.getenv("MONGO_DATABASE_URI")
 
 uri = MONGO_DATABASE_URI
-slave = MONGO_SLAVE_URI
 
 client = MongoClient(uri)
-client_slave = MongoClient(slave)
 
 db = client.admin
-db_slave = client_slave.admin
 
-print("table names", db.list_collection_names())
 # inserting 10 database sets in 5 random servers
 for _ in range(5):
     database = fake.database()
     posts = db[database]
-    posts_slave = db_slave[database]
 
     for _ in range(10):
         print(fake.name())
@@ -52,12 +46,7 @@ for _ in range(5):
         post_id = posts.insert_one(EMPLOYEE).inserted_id
     # print all values in all used tables
     for post in posts.find():
-        print("Values inside table primary server", post)
-    for posts_slave in posts_slave.find():
-        print("values inside tables secondary servers", posts_slave)
+        print("Values inserted inside " + database + " table", post)
 
 # check for created tables
-print("table names in primary server", db.list_collection_names())
-
-# checking all the other servers
-print("table names in other servers", db_slave.list_collection_names())
+print("table names", db.list_collection_names())
